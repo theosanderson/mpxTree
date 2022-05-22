fb2d8a106e6b608750fccb67d70a8d9e3b31898e
Theo Sanderson <theo@sndrsn.co.uk>
2022-05-22 19:40:23 +0000

    update

--- build_tree.sh
+++ build_tree.sh
@@ -1,3 +1,4 @@
 curl https://mpox-lapis.gen-spectrum.org/v1/sample/fasta-aligned?downloadAsFile=true -o out.fa
+grep -v 
 ./FastTree -nt -gtr out.fa > out.nwk
 wget https://mpox-lapis.gen-spectrum.org/v1/sample/details?downloadAsFile=true&dataFormat=csv -O out.csv
