f0906b8314a47370e608159408509268a9afdd7e
Theo Sanderson <theo@sndrsn.co.uk>
2022-05-24 09:50:50 +0000

    update

--- usher_stuff.sh
+++ usher_stuff.sh
@@ -5,5 +5,5 @@ usher -t rooted.nwk --vcf merged.vcf --collapse-tree --save-mutation-annotated-t
 pip install -U taxoniumtools
 wget -N https://raw.githubusercontent.com/nextstrain/monkeypox/master/config/reference.gb
 wget "https://mpox-lapis.gen-spectrum.org/v1/sample/details?downloadAsFile=true&dataFormat=csv" -O metadata.csv
-usher_to_taxonium -i mpx.pb -g reference.gb -o mpx.jsonl.gz -m metadata.csv --columns date,country,region,clade,host,sraAccession --only_variable_sites
+usher_to_taxonium -i mpx.pb -g reference.gb -o mpx2.jsonl.gz -m metadata.csv --columns date,country,region,clade,host,sraAccession --only_variable_sites
 
