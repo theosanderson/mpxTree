f71f5481fe6942cf8808fd59810cec98314eeb0b
Theo Sanderson <theo@sndrsn.co.uk>
2022-05-30 13:45:15 +0000

    update

--- initial.sh
+++ initial.sh
@@ -4,8 +4,12 @@ pip install Bio
 python mask.py -f out.fa -b mask.bed -o masked.fa
 # convert fasta to tsv
 wget -N https://raw.githubusercontent.com/nextstrain/monkeypox/master/config/exclude_overview.txt
+wget -N https://raw.githubusercontent.com/nextstrain/monkeypox/master/config/exclude.txt
+wget -N https://raw.githubusercontent.com/nextstrain/monkeypox/master/config/exclude_outbreak.txt
+cat exclude* > all_exclude.txt
 
-./fasta2tbl masked.fa | grep -v -f exclude_overview.txt  > masked.tsv
+
+./fasta2tbl masked.fa | grep -v -f all_exclude.txt  > masked.tsv
 ./tbl2fasta masked.tsv > masked.fa
 
 sed -i "s/[']//g" masked.fa
