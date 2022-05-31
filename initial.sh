f00ae378d1db391371ea4ef8cbba25cfbdaeb2f5
Theo Sanderson <theo@sndrsn.co.uk>
2022-05-31 16:33:06 +0000

    update

--- initial.sh
+++ initial.sh
@@ -9,7 +9,8 @@ wget -N https://raw.githubusercontent.com/nextstrain/monkeypox/master/config/exc
 cat exclude* > all_exclude.txt
 # remove anything after a hash in all_exclude.txt
 sed -i 's/ *\#.*//' all_exclude.txt
-
+# remove empty lines
+sed -i '/^$/d' all_exclude.txt
 
 ./fasta2tbl masked.fa | grep -v -f all_exclude.txt  > masked.tsv
 ./tbl2fasta masked.tsv > masked.fa
