33358f43dc7c0675e1b7c9648167d13ce154554c
Theo Sanderson <theo@sndrsn.co.uk>
2022-05-31 16:55:21 +0000

    upd

--- initial.sh
+++ initial.sh
@@ -5,7 +5,7 @@ python mask.py -f out.fa -b mask.bed -o masked.fa
 # convert fasta to tsv
 wget -N https://raw.githubusercontent.com/nextstrain/monkeypox/master/config/exclude_overview.txt
 wget -N https://raw.githubusercontent.com/nextstrain/monkeypox/master/config/exclude.txt
-wget -N https://raw.githubusercontent.com/nextstrain/monkeypox/master/config/exclude_outbreak.txt
+
 cat exclude* > all_exclude.txt
 # remove anything after a hash in all_exclude.txt
 sed -i 's/ *\#.*//' all_exclude.txt
