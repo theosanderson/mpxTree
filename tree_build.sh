d4485e88c56c9f619e5fc55f318e44db3cc8f4c3
Theo Sanderson <theo@sndrsn.co.uk>
2022-06-03 16:48:23 +0000

    update

--- tree_build.sh
+++ tree_build.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-./raxml-ng --msa masked.fa --threads 2  --model GTR+G -prefix raxml/out.fa --redo
+./raxml-ng --msa masked.fa --threads 2  --model GTR+G -prefix raxml/out.fa --redo --tree pars2,rand2
