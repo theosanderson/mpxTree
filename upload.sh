0b8828fcafb7e61f5ba8f1fc86a9b4f257e2978a
Theo Sanderson <theo@sndrsn.co.uk>
2022-10-05 15:25:00 +0100

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,4 +1,4 @@
-sed  "s/MYSECRETKEY/$MYKEY/g" s3cfg > s3cfg
-s3cmd put -P mpx.jsonl.gz -c s3cfg s3://cov2tree/mpx.jsonl.gz 
+sed  "s/MYSECRETKEY/$MYKEY/g" s3cfg > s3cfg2
+s3cmd put -P mpx.jsonl.gz -c s3cfg2 s3://cov2tree/mpx.jsonl.gz 
 
 
