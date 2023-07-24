bac6f9a4de806230bef1323efe52483958c940c6
Theo Sanderson <theo@sndrsn.co.uk>
2023-07-24 16:46:04 +0100

    Update usher_stuff.sh

--- usher_stuff.sh
+++ usher_stuff.sh
@@ -7,4 +7,4 @@ pip install -U taxoniumtools
 wget -N https://raw.githubusercontent.com/nextstrain/monkeypox/master/config/reference.gb
 wget "https://mpox-lapis.genspectrum.org/v1/sample/details?downloadAsFile=true&dataFormat=csv" -O metadata.csv
 usher_to_taxonium -i mpx.pb -g NC_063383.gb -o mpx.jsonl.gz -m metadata.csv --columns date,strain,country,region,clade,host,institution --only_variable_sites --key_column=accession
-rm masked.fa out.fa masked.tsv merged.vcf
+rm masked.fa out.fa masked.tsv 
